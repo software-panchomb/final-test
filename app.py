e49cac1c3fc88ec567f1b7fdce2effd97aaa3f5c
panchomb <francisco.magot@utec.edu.pe>
2022-07-12 19:30:10 -0500

    func2

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import random
+
 def func():
     print("hello world")
 
 
 
-    x = 1
+    x = random.randint(0,1)
     if x == 1:
         print('hey')
+    else:
+        print('no entry')
 
     return x
+
+def func2():
+
+    integer = 10
+
+    if integer % 2 == 0:
+        integer += 1
+
+    return integer
